bc6eda3083d6557403686473aad775fb50c1c28a
USER <USER@DESKTOP-1SRKQIR>
2020-02-22 00:05:22 +0900

    HelloGitTest 수정

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -7,6 +7,10 @@ public class HelloGitTest {
 		System.out.println("안녕하세요``!");
 		System.out.println("축하드립니다. commit&push!!!");
 		System.out.println("축드립니다. commit&push!!!");
+		System.out.println("축드fasdfsad립니다. commit&push!!!");
+		System.out.println("축드립니sdfsdf다. commit&push!!!");
+		System.out.println("축드립니fsdfsdf다. commit&push!!!");
+		System.out.println("축드립니sdsdfsdf다. commit&push!!!");
 	}
 
 }
