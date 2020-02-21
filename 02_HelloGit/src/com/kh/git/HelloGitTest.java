3878642e479a6c4fbf30c7b74f9446346f096a1c
shinhyungchul <vkoreav11@naver.com>
2020-02-22 00:41:19 +0900

    수정4

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -5,7 +5,7 @@ public class HelloGitTest {
 	public static void main(String[] args) {
 		System.out.println("수정수정수정!");
 		System.out.println("야야야야야야야야``!");
-		System.out.println("축하드립니다. commit&push!!!");
+		System.out.println("안축하합니다. commit&push!!!");
 		System.out.println("aaaaa형철바보");
 		System.out.println("aa`뻐큐먹어`!");
 		System.out.println("aasdasdaa``!");
