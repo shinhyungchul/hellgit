a9b1425d03d84f017df40503aaf4c04ee09576a7
USER <USER@DESKTOP-1SRKQIR>
2020-02-22 00:12:06 +0900

    수정수정수정

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -3,8 +3,8 @@ package com.kh.git;
 public class HelloGitTest {
 
 	public static void main(String[] args) {
-		System.out.println("안녕하세요!");
-		System.out.println("안녕하세요``!");
+		System.out.println("수정수정수정!");
+		System.out.println("수정수정수정``!");
 		System.out.println("축하드립니다. commit&push!!!");
 		System.out.println("축드립니다. commit&push!!!");
 		System.out.println("축드fasdfsad립니다. commit&push!!!");
