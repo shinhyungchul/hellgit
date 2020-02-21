ade0af304039e3817ff7310bb906bce67b3f16fe
USER <USER@DESKTOP-1SRKQIR>
2020-02-22 00:10:39 +0900

    0222뷰단수정1 형철

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -8,8 +8,6 @@ public class HelloGitTest {
 		System.out.println("축하드립니다. commit&push!!!");
 		System.out.println("축드립니다. commit&push!!!");
 		System.out.println("축드fasdfsad립니다. commit&push!!!");
-		System.out.println("축드립니sdfsdf다. commit&push!!!");
-		System.out.println("축드립니fsdfsdf다. commit&push!!!");
 		System.out.println("축드립니sdsdfsdf다. commit&push!!!");
 	}
 
