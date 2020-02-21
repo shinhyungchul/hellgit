27b3f3af53ccbf720d03c14b5433c8a23bb0c8b2
jekakYeon1214 <gza9112@gmail.com>
2020-02-22 00:11:08 +0900

    jekak_commit2

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -4,8 +4,8 @@ public class HelloGitTest {
 
 	public static void main(String[] args) {
 		System.out.println("신형철바보");
-		System.out.println("멍청이``!");
-		System.out.println("축하드립니다. commit&push!!!");
+		System.out.println("aa``!");
+		System.out.println("똥꼬다. commit&push!!!");
 		System.out.println("축드립니다. commit&push!!!");
 	}
 
